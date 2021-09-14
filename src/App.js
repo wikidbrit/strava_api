9c8147fe46b9d3609c06a78b7387b1f8ac5ba90b
wikidbrit <pflemingonline@gmail.com>
2021-09-14 11:45:26 +0200

    test map working with tesla local data

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import "./App.css";
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+import tesla from "./data/tesla.json";
+
 function App() {
+  const filteredStations = tesla.filter(tsla => tsla.address.country === 'Sweden')
 
   return (
     <div className="App">
       <h1>Hello Woot</h1>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+
+      <MapContainer center={[59.334591, 18.063240]} zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
 
+        {filteredStations.map((test) => (
+          <Marker 
+          key={test.id}
+          position={[test.gps.latitude, test.gps.longitude]}
+          >
+
+            <Popup position={[test.gps.latitude, test.gps.longitude]}>
+              <div>
+                <h2>{"Name: " + test.name}</h2>
+                <p>{"Status: " + test.status}</p>
+                <p>{"Stall Count: " + test.stallCount}</p>
+              </div>
+            </Popup>
+
+          </Marker>
+
+        ))}
+      </MapContainer>
     </div>
   );
 }
